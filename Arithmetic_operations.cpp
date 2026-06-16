12b4ba7f40f06a0389675c80b2ae73c7071c6bc0
Ninson Aaron 01243754b <ninsonaaron19@gmail.com>
2026-06-16 23:09:43 +0000

    add program to perform arithmetic operation on two integers

---
+++ Arithmetic_operations.cpp
@@ -0,0 +1,16 @@
+# include <iostream> 
+
+using namespace std;
+int main(){
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "sum: " << a + b << endl;
+    cout << "difference: " << a - b << endl;
+    cout << "product: " << a * b << endl;
+    if (b != 0) {
+        cout << "quotient: " << a / b << endl;
+    } else {
+        cout << "cannot divide by zero" << endl;
+    } return 0;
+    }
